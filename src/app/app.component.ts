afc1ef05fbe5c0cc9274d537c6693b2d5988cef5
GutyLabs <gutylabs20@gmail.com>
2021-01-20 22:47:36 -0500

    Formulario : Registro Terminado. Falta la edición

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './models/empleado';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajador';
+
+  posicion: number = -1;
+
+  actual:Empleado = new Empleado();
+
+  empleados = [
+    new Empleado( 'Juan', 'USA'),
+    new Empleado( 'Pedro', 'USA'),
+    new Empleado( 'Luis', 'USA'),
+  ];
+
+  registrar():void{
+
+    if (this.posicion == -1) {
+      this.empleados.push(this.actual);
+    } else {
+      this.empleados[this.posicion].nombre = this.actual.nombre;
+      this.empleados[this.posicion].pais = this.actual.pais;
+    }
+
+  }
+
 }
--- src/app/models/empleado.ts
+++ src/app/models/empleado.ts
@@ -1,2 +1,13 @@
 export class Empleado {
+
+  nombre: string;
+  pais: string;
+
+  constructor(nombre:string='', pais:string=''){
+
+    this.nombre=nombre;
+    this.pais=pais;
+
+  }
+
 }
